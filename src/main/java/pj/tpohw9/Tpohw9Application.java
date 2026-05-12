c6c32ed4fc300d2500c8d3ade609da1c3bd95fc9
Nikita Pohrebniak <s32876@pjwstk.edu.pl>
2026-05-12 10:41:51 +0200

    Initial commit

---
+++ src/main/java/pj/tpohw9/Tpohw9Application.java
@@ -0,0 +1,13 @@
+package pj.tpohw9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpohw9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tpohw9Application.class, args);
+    }
+
+}
